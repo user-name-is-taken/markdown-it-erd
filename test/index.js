e1fee0a9e5f91104bd646a2c7950c76337761a74
Michael Lundquist <michaellundquist7@gmail.com>
2019-12-31 12:48:22 -0500

    Simplest version of writing erd to an svg file, then reading that file back out.

--- test/index.js
+++ test/index.js
@@ -1,79 +1,79 @@
 const erd = require('erd')
+let fs = require("fs");
+let path = require("path");
 
-//TITLE NOT SUPPORTED
-erd({
-  modelsText: `
-  # Entities
-  [Action] {bgcolor: "#d0e0d0"}
-  *_id [ ObjectId ]
-  struct [ ObjectId ]
-  type [ String ]
-  label [ String , not null ]
-  name [ String , not null ]
-  description [ String , not null ]
-  behavior [ String , not null ]
-  content [ String ]
-  successMessage [ String ]
-  successPage [ ObjectId ]
-  icon [ String ]
-  
-  [exception] {bgcolor: "#d0e0d0"}
-  *_id [ ObjectId ]
-  name [ String , not null ]
-  type [ ObjectId ]
-  
-  [namespace] {bgcolor: "#d0e0d0"}
-  *_id [ ObjectId ]
-  serviceName [ String , not null ]
-  
-  [Struct] {bgcolor: "#d0e0d0"}
-  *_id [ ObjectId ]
-  name [ String , not null ]
-  fields [ ObjectId ]
-  
-  [thrift] {bgcolor: "#d0e0d0"}
-  *_id [ ObjectId ]
-  name [ String , not null ]
-  
-  [typedef] {bgcolor: "#d0e0d0"}
-  *_id [ ObjectId ]
-  name [ String , not null ]
-  type [ String , not null ]
-  
-  [Layout] {bgcolor: "#d0e0d0"}
-  *_id [ ObjectId ]
-  name [ String , not null ]
-  title [ String , not null ]
-  type [ String , not null ]
-  fields [ ObjectId ]
-  struct [ ObjectId ]
-  api [ String ]
-  actions [ ObjectId ]
-  
-  [Menu] {bgcolor: "#d0e0d0"}
-  *_id [ ObjectId ]
-  name [ String , not null ]
-  type [ String , not null ]
-  page [ ObjectId ]
-  content [ String , not null ]
-  project [ ObjectId ]
-  
-  [Page] {bgcolor: "#d0e0d0"}
-  *_id [ ObjectId ]
-  title [ String ]
-  layouts [ ObjectId ]
-  params [ ObjectId ]
-  queryString [ String ]
-  
-  [Project] {bgcolor: "#d0e0d0"}
-  *_id [ ObjectId ]
-  name [ String , not null ]
-  description [ String ]
-  logo [ String ]
-  collaborators [ Array<ObjectId> ]
-  teams [ Array<ObjectId> ]
+let erdCode = `
+# Entities
+[Action] {bgcolor: "#d0e0d0"}
+*_id [ ObjectId ]
+struct [ ObjectId ]
+type [ String ]
+label [ String , not null ]
+name [ String , not null ]
+description [ String , not null ]
+behavior [ String , not null ]
+content [ String ]
+successMessage [ String ]
+successPage [ ObjectId ]
+icon [ String ]
 
-  [Team] {bgcolor: "#d0e0d0"}
+[exception] {bgcolor: "#d0e0d0"}
+*_id [ ObjectId ]
+name [ String , not null ]
+type [ ObjectId ]
+
+[namespace] {bgcolor: "#d0e0d0"}
+*_id [ ObjectId ]
+serviceName [ String , not null ]
+
+[Struct] {bgcolor: "#d0e0d0"}
+*_id [ ObjectId ]
+name [ String , not null ]
+fields [ ObjectId ]
+
+[thrift] {bgcolor: "#d0e0d0"}
+*_id [ ObjectId ]
+name [ String , not null ]
+
+[typedef] {bgcolor: "#d0e0d0"}
+*_id [ ObjectId ]
+name [ String , not null ]
+type [ String , not null ]
+
+[Layout] {bgcolor: "#d0e0d0"}
+*_id [ ObjectId ]
+name [ String , not null ]
+title [ String , not null ]
+type [ String , not null ]
+fields [ ObjectId ]
+struct [ ObjectId ]
+api [ String ]
+actions [ ObjectId ]
+
+[Menu] {bgcolor: "#d0e0d0"}
+*_id [ ObjectId ]
+name [ String , not null ]
+type [ String , not null ]
+page [ ObjectId ]
+content [ String , not null ]
+project [ ObjectId ]
+
+[Page] {bgcolor: "#d0e0d0"}
+*_id [ ObjectId ]
+title [ String ]
+layouts [ ObjectId ]
+params [ ObjectId ]
+queryString [ String ]
+
+[Project] {bgcolor: "#d0e0d0"}
+*_id [ ObjectId ]
+name [ String , not null ]
+description [ String ]
+logo [ String ]
+collaborators [ Array<ObjectId> ]
+teams [ Array<ObjectId> ]
+
+[Team] {bgcolor: "#d0e0d0"}
 *_id [ ObjectId ]
 +name [ String , not null ]
 description [ String ]
@@ -101,4 +101,9 @@ Menu *--* Project
 Page *--* Layout
 Team *--* User
 User *--* Team
-`, outputType: "html"})
+`;
+
+//TITLE NOT SUPPORTED
+erd({ modelsText: erdCode, outputType: "html"})
+
+console.log(fs.readFileSync(path.join(__dirname, "../erd.html"), { encoding: 'utf8' })); // zzzz....
